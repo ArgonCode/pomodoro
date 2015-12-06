becbcf2e36f64a69d04cf5287ce656acbe4fcb1d
Sylwia M. Bialczak <argoncode18@gmail.com>
2015-12-06 14:07:19 -0800

    Display countdown on document.ready.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-/*
-1. Get timer to display and run down
-2. Stop timer on click. Reset. In a true fasion of Pomodoro, break cancels it all.
-3.
-
-1. get now moment.time
-2. add desired timer
-3. display moments. untill the time in the future, on repeat
-
-
-
-*/
-
 // set time in the future
 var timeNow = new Date();
 var timerMinutes = 25;
@@ -23,6 +10,13 @@ function countDown() {
   var timeLeft = timerUTC - Date.parse(new Date());
   var timeLeftMinutes = Math.floor(timeLeft / 60000);
   var timeLeftSeconds = (timeLeft - (timeLeftMinutes * 60000)) / 1000;
-
-  return "" + timeLeftMinutes + ":" + timeLeftSeconds;
+  $("#countdown").html(timeLeftMinutes + ":" + timeLeftSeconds);
+  // return "" + timeLeftMinutes + ":" + timeLeftSeconds;
 }
+
+
+$(document).ready(function() {
+  setInterval(function(){
+    countDown();
+  },1000);
+});
